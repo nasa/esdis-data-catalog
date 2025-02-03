7e9d75e9427260c4013ef5af29fc4e8b4a6101cc
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2025-02-03 16:10:55 -0500

    DCAT-2: Add doc-strings for the constants

--- src/ts/constants/sortKeys.ts
+++ src/ts/constants/sortKeys.ts
@@ -1,5 +1,12 @@
+/**
+ * The default sort key used for sorting collections.
+ */
 export const defaultSortKey = 'relevance'
 
+/**
+ * An array of objects representing the available sort keys for collections.
+ * Each object contains a `key` which is used for sorting and a `value` which is the display name.
+ */
 export const collectionSortKeys = [
   {
     key: 'relevance',
